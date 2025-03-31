c901020e4d27f2c70f28e26afeff585851f44675
Petar Kresoja <pkresoja@singidunum.ac.rs>
2025-03-31 15:38:56 +0200

    Installed required dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-console.log('1')
-console.log('2')
-console.log(3)
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const app = express()
+app.use(cors())
+app.use(morgan('tiny'))
+
+app.get('/', (req, res) => {
+    res.send('hi')
+})
+
+app.listen(3000, () => console.log('app started'))
